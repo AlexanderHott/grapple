8c5fee070acf9029e4414eab033ee6a50043928d
AlexanderHott <66271487+AlexanderHott@users.noreply.github.com>
2026-01-13 12:06:24 -0800

    recreate swapchain on resize or when vulkan says to

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@
     unsafe_op_in_unsafe_fn,
     clippy::too_many_arguments,
     clippy::unnecessary_wraps,
-    clippy::unnecessary_return
+    clippy::needless_return,
+
 )]
 use anyhow::{Result, anyhow};
 use log::*;
@@ -17,14 +18,13 @@ use vulkanalia::bytecode::Bytecode;
 use vulkanalia::loader::{LIBRARY, LibloadingLoader};
 use vulkanalia::prelude::v1_0::*;
 use vulkanalia::vk::KhrSurfaceExtensionInstanceCommands;
+use vulkanalia::vk::KhrSwapchainExtensionDeviceCommands;
 use vulkanalia::vk::{ExtDebugUtilsExtensionInstanceCommands, PhysicalDevice};
 use vulkanalia::window as vk_window;
 use winit::dpi::LogicalSize;
 use winit::event::{Event, WindowEvent};
 use winit::event_loop::EventLoop;
 use winit::window::{Window, WindowBuilder};
-
-use vulkanalia::vk::KhrSwapchainExtensionDeviceCommands;
 
 const PORTABILITY_MACOS_VERSION: Version = Version::new(1, 3, 216);
 
@@ -53,10 +53,19 @@ fn main() -> Result<()> {
     // SAFETY:
     // 1. nothing yet
     let mut app = unsafe { App::create(&window)? };
+    let mut minimized = false;
     event_loop.run(move |event, event_loop_window_target| match event {
         Event::AboutToWait => window.request_redraw(),
         Event::WindowEvent { event, .. } => match event {
-            WindowEvent::RedrawRequested if !event_loop_window_target.exiting() => {
+            WindowEvent::Resized(size) => {
+                if size.width == 0 && size.height == 0 {
+                    minimized = true;
+                } else {
+                    minimized = false;
+                    app.resized = true
+                }
+            }
+            WindowEvent::RedrawRequested if !event_loop_window_target.exiting() && !minimized => {
                 unsafe { app.render(&window) }.unwrap()
             }
             WindowEvent::CloseRequested => {
@@ -80,6 +89,7 @@ struct App {
     data: AppData,
     device: Device,
     frame: usize,
+    resized: bool,
 }
 
 impl App {
@@ -111,6 +121,7 @@ impl App {
             data,
             device,
             frame: 0,
+            resized: false,
         });
     }
 
@@ -120,29 +131,25 @@ impl App {
         const TIMEOUT_NONE: u64 = u64::MAX;
 
         let in_flight_fence = self.data.in_flight_fences[self.frame];
-        self.device.wait_for_fences(
-            &[in_flight_fence],
-            WAIT_FOR_ALL_FENCES,
-            TIMEOUT_NONE,
-        )?;
+        self.device
+            .wait_for_fences(&[in_flight_fence], WAIT_FOR_ALL_FENCES, TIMEOUT_NONE)?;
 
-        let image_index = self
-            .device
-            .acquire_next_image_khr(
-                self.data.swapchain,
-                TIMEOUT_NONE,
-                self.data.image_available_semaphores[self.frame],
-                vk::Fence::null(),
-            )?
-            .0 as usize;
+        let result = self.device.acquire_next_image_khr(
+            self.data.swapchain,
+            TIMEOUT_NONE,
+            self.data.image_available_semaphores[self.frame],
+            vk::Fence::null(),
+        );
+        let image_index = match result {
+            Ok((image_index, _)) => image_index as usize,
+            Err(vk::ErrorCode::OUT_OF_DATE_KHR) => return self.recreate_swapchain(window),
+            Err(e) => return Err(anyhow!(e)),
+        };
 
         let image_in_flight = self.data.images_in_flight[image_index];
         if !image_in_flight.is_null() {
-            self.device.wait_for_fences(
-                &[image_in_flight],
-                WAIT_FOR_ALL_FENCES,
-                TIMEOUT_NONE,
-            )?;
+            self.device
+                .wait_for_fences(&[image_in_flight], WAIT_FOR_ALL_FENCES, TIMEOUT_NONE)?;
         }
 
         self.data.images_in_flight[image_index] = in_flight_fence;
@@ -157,16 +164,11 @@ impl App {
             .command_buffers(command_buffers)
             .signal_semaphores(signal_semaphores);
 
-
         self.device
             .reset_fences(&[self.data.in_flight_fences[self.frame]])?;
 
-
-        self.device.queue_submit(
-            self.data.graphics_queue,
-            &[submit_info],
-            in_flight_fence,
-        )?;
+        self.device
+            .queue_submit(self.data.graphics_queue, &[submit_info], in_flight_fence)?;
 
         let swapchains = &[self.data.swapchain];
         let image_indices = &[image_index as u32];
@@ -175,8 +177,18 @@ impl App {
             .swapchains(swapchains)
             .image_indices(image_indices);
 
-        self.device
-            .queue_present_khr(self.data.present_queue, &present_info)?;
+        let result = self
+            .device
+            .queue_present_khr(self.data.present_queue, &present_info);
+        let changed = result == Ok(vk::SuccessCode::SUBOPTIMAL_KHR)
+            || result == Err(vk::ErrorCode::OUT_OF_DATE_KHR);
+
+        if self.resized || changed {
+            self.resized = false;
+            self.recreate_swapchain(window)?;
+        } else if let Err(e) = result {
+            return Err(anyhow!(e));
+        }
 
         self.frame = (self.frame + 1) % MAX_FRAMES_IN_FLIGHT;
 
@@ -185,6 +197,7 @@ impl App {
 
     /// Destroy our Vulkan app.
     unsafe fn destroy(&mut self) {
+        self.destroy_swapchain();
         self.data
             .in_flight_fences
             .iter()
@@ -199,10 +212,41 @@ impl App {
             .for_each(|s| self.device.destroy_semaphore(*s, None));
         self.device
             .destroy_command_pool(self.data.command_pool, None);
+        self.device.destroy_device(None);
+        self.instance.destroy_surface_khr(self.data.surface, None);
+
+        if VALIDATION_ENABLED {
+            self.instance
+                .destroy_debug_utils_messenger_ext(self.data.messenger, None);
+        }
+        self.instance.destroy_instance(None);
+    }
+
+    unsafe fn recreate_swapchain(&mut self, window: &Window) -> Result<()> {
+        self.device.device_wait_idle()?;
+        self.destroy_swapchain();
+        create_swapchain(window, &self.instance, &self.device, &mut self.data)?;
+        create_swapchain_image_views(&self.device, &mut self.data)?;
+        create_render_pass(&self.instance, &self.device, &mut self.data)?;
+        create_pipeline(&self.device, &mut self.data)?;
+        create_framebuffers(&self.device, &mut self.data)?;
+        create_command_buffers(&self.device, &mut self.data)?;
+        self.data
+            .images_in_flight
+            .resize(self.data.swapchain_images.len(), vk::Fence::null());
+
+        return Ok(());
+    }
+
+    unsafe fn destroy_swapchain(&mut self) {
         self.data
             .framebuffers
             .iter()
             .for_each(|f| self.device.destroy_framebuffer(*f, None));
+
+        self.device
+            .free_command_buffers(self.data.command_pool, &self.data.command_buffers);
+        self.device.destroy_pipeline(self.data.pipeline, None);
         self.device
             .destroy_pipeline_layout(self.data.pipeline_layout, None);
         self.device.destroy_render_pass(self.data.render_pass, None);
@@ -211,14 +255,6 @@ impl App {
             .iter()
             .for_each(|v| self.device.destroy_image_view(*v, None));
         self.device.destroy_swapchain_khr(self.data.swapchain, None);
-        self.device.destroy_device(None);
-
-        if VALIDATION_ENABLED {
-            self.instance
-                .destroy_debug_utils_messenger_ext(self.data.messenger, None);
-        }
-        self.instance.destroy_surface_khr(self.data.surface, None);
-        self.instance.destroy_instance(None);
     }
 }
 
@@ -467,7 +503,7 @@ unsafe fn create_swapchain(
 
     let surface_format = get_swapchain_surface_format(&support.formats);
     let present_mode = get_swapchain_present_mode(&support.present_modes);
-    let extent = get_swapchain_extent(&window, &support.capabilities);
+    let extent = get_swapchain_extent(window, &support.capabilities);
 
     // Add one to prevent possibly waiting on the device
     // https://kylemayes.github.io/vulkanalia/presentation/swapchain.html
